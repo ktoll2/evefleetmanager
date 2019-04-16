11a4668449253913514ede93cf95accfa987ac1a
Kirk Tolleshaug <kirk.tolleshaug@vizientinc.com>
2019-04-16 13:14:04 -0500

    init

---
+++ EveFleetManager/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EveFleetManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
