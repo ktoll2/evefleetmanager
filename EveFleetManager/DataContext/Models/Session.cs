e5b1e8e5aa22d15a77805fced7223fc72ba3c28a
Kirk Tolleshaug <kirk.tolleshaug@vizientinc.com>
2019-05-01 16:22:24 -0500

    begin adding db

---
+++ EveFleetManager/DataContext/Models/Session.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EveFleetManager.DataContext.Models
+{
+    public class Session
+    {
+        public long characterId { get; set; }
+        public DateTime sessionExpires { get; set; }
+        public string sessionId { get; set; }
+
+    }
+}
